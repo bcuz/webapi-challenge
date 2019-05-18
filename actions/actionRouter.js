770f3acfdac9b72e3aefe979fafdcfd9026f147c
adam <bcuz@users.noreply.github.com>
2019-05-18 14:29:06 -0400

    forgot update an action

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -72,6 +72,27 @@ router.post('/', async (req, res) => {
   
 });
 
+router.put('/:id', async (req, res) => {
+  if(Object.keys(req.body).length === 0){
+    res.status(400).json({message: "missing data"});
+  }
+
+  try {
+    const action = await Actions.update(req.params.id, req.body);
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: 'The action could not be found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the action',
+    });
+  }
+});
+
 router.delete('/:id', async (req, res) => {
   // might refactor later.
   try {
