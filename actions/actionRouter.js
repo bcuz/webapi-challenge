6dd73f7c1134cbe0c04a29ca2c90f13293124806
adam <bcuz@users.noreply.github.com>
2019-05-18 13:16:43 -0400

    get action

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -17,4 +17,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  // might refactor later
+  try {
+    const action = await Actions.get(req.params.id);  
+
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: 'action not found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the action',
+    });
+  }  
+});
+
 module.exports = router;
